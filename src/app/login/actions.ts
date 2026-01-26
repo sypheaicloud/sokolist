470f8b4565b00998ac7ad260cd68614888c79c46
JosiahBoziah <sypheit@gmail.com>
2026-01-26 00:29:54 -0600

    fix: add redirect to home page after successful login

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -12,7 +12,10 @@ export async function authenticate(
     formData: FormData,
 ) {
     try {
-        await signIn('credentials', formData);
+        await signIn('credentials', {
+            ...Object.fromEntries(formData),
+            redirectTo: '/',
+        });
     } catch (error) {
         if (error instanceof AuthError) {
             switch (error.type) {
